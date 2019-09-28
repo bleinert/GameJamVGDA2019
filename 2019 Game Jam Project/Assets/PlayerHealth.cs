e9f38095ec1d6c68b5f125b636d1ed2f5aba32c0
Michael <michael.zatlin@yahoo.com>
2019-09-28 11:20:05 -0700

    Added in Player Stats, Basic Health Interface

---
+++ 2019 Game Jam Project/Assets/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IHealth
+{
+
+    [SerializeField]
+    PlayerStatsSO stats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecieveDamage(float damage)
+    {
+
+        throw new System.NotImplementedException();
+    }
+}
