37902bdbb83c45e0cf2db995fc7b3328ca3d96df
bleinert <bleinert@gmail.com>
2019-09-28 11:03:53 -0700

    Player controller component has been created!

---
+++ 2019 Game Jam Project/Assets/player_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 10;
+    float characteraccel = 1.1f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * characteraccel * Time.fixedDeltaTime);
+    }
+}
