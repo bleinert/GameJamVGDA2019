e4d7ae914558477820bb514a637bcd56ee0f1924
Michael <michael.zatlin@yahoo.com>
2019-09-28 11:32:11 -0700

    PlayerHealth Test

--- 2019 Game Jam Project/Assets/PlayerHealth.cs
+++ 2019 Game Jam Project/Assets/PlayerHealth.cs
@@ -7,6 +7,11 @@ public class PlayerHealth : MonoBehaviour, IHealth
 
     [SerializeField]
     PlayerStatsSO stats;
+    float health = 1;
+    public Health{
+        get{ return health;}
+        set{ health = Ke
+    }
 
     // Start is called before the first frame update
     void Start()
