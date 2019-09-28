4e9f3e585b1dac987323cdc358eb9b2aaa4646cb
TonyAMO <aojeda4211@gmail.com>
2019-09-28 15:10:20 -0700

    assets

---
+++ 2019 Game Jam Project/Assets/EnMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnMovement : MonoBehaviour
+{
+
+    float constantSpeed = 200f;
+    public Rigidbody2D rb;
+
+
+    // Update is called once per frame
+    void Start(){
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        rb.velocity = (constantSpeed) * (rb.velocity);
+    }
+}
