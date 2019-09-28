9f9c0d924c1236932eda0bd37959afd1d8731674
Michael <michael.zatlin@yahoo.com>
2019-09-28 14:52:24 -0700

    Basics of Player Shooting

--- 2019 Game Jam Project/Assets/playerShoot.cs
+++ 2019 Game Jam Project/Assets/playerShoot.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerShoot : MonoBehaviour
+public class PlayerShoot : MonoBehaviour
 {
+    [SerializeField]
+    Bullet bullet;
+    [SerializeField]
+    Vector2 offset = new Vector2(0.2f, 0.2f);
+    float delay = 0f;
+    [SerializeField]
+    float delayMax = .3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        delay = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            if (delay > delayMax)
+            {
+                Debug.Log("Shoot");
+              //  Instantiate(bullet, transform.position, Quaternion.identity);
+                delay = 0;
+            }
+
+        }
+        delay += Time.fixedDeltaTime;
     }
 }
