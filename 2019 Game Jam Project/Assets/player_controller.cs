dbd81c46a847799fbfd1c91b056afaacecc22e35
Michael <michael.zatlin@yahoo.com>
2019-09-28 12:59:46 -0700

    Updated Player Movement

--- 2019 Game Jam Project/Assets/player_controller.cs
+++ 2019 Game Jam Project/Assets/player_controller.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class player_controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    float speed = 10;
-    float characteraccel = 1.1f;
+    float speed = 1f;
+    float characteraccel = 1.0f;
     public Rigidbody2D rb;
     Vector2 movement;
+    [SerializeField]
+    float flySpeed = 1f;
+    Vector2 flyVector;
     
     void Start()
     {
@@ -18,13 +21,14 @@ public class player_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");
+    //    movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
+        flyVector = new Vector2(1 * flySpeed*Time.fixedDeltaTime, 0);
     }
 
     private void FixedUpdate()
     {
+        movement += flyVector;
         rb.MovePosition(rb.position + movement * speed * characteraccel * Time.fixedDeltaTime);
     }
 }
