581846a355a93191a8216e022b237b695708d498
TonyAMO <aojeda4211@gmail.com>
2019-09-28 16:39:17 -0700

    Menu start and quit, next scene

---
+++ 2019 Game Jam Project/Assets/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour { 
+
+    // Update is called once per frame
+    public void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
