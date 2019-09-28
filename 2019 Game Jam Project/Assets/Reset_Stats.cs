19c778b543b6b1e2b0d3caa24487b92d90d341a0
bleinert <bleinert@gmail.com>
2019-09-28 14:48:58 -0700

    Added reset stats script

---
+++ 2019 Game Jam Project/Assets/Reset_Stats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset_Stats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerStatsSO playerstats;
+
+    void Start()
+    {
+        playerstats.playerFuel= 1;
+        playerstats.playerHealth = 1;
+        playerstats.playerSpeed = 1;
+
+    }
+
+}
