c319b8fec8b12851c5043866f2ace2a73721df03
bleinert <bleinert@gmail.com>
2019-09-28 17:07:45 -0700

    added fuel bar, fixed rotation mechanic

---
+++ 2019 Game Jam Project/Assets/FuelBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour
+{
+    public Slider fuelbarslider;
+    public Text TxtFuel;
+    public int Min;
+    public int Max;
+    private float mCurrentValue;
+    private float mCurrentPercent;
+    public PlayerStatsSO playerstats;
+
+    public void SetFuel(float fuel)
+    {
+        if (fuel != mCurrentValue) {
+            if (Max - Min == 0) {
+                mCurrentValue = 0;
+                mCurrentPercent = 0;
+            }
+            else
+            {
+                mCurrentValue = fuel;
+                mCurrentPercent = mCurrentValue / (float)(Max - Min);
+            }
+            TxtFuel.text = string.Format("{0} %", Mathf.RoundToInt(mCurrentPercent * 100));
+            fuelbarslider.value = mCurrentPercent;
+        }
+    }
+
+    private void Update()
+    {
+        SetFuel(playerstats.playerFuel);
+
+    }
+
+}
--- 2019 Game Jam Project/Assets/Reset_Stats.cs
+++ 2019 Game Jam Project/Assets/Reset_Stats.cs
@@ -6,12 +6,14 @@ public class Reset_Stats : MonoBehaviour
 {
     // Start is called before the first frame update
     public PlayerStatsSO playerstats;
-
+    public 
     void Start()
     {
         playerstats.playerFuel= 1;
-        playerstats.playerHealth = 1;
+        playerstats.playerHealth = 100;
         playerstats.playerSpeed = 1;
+
+
 
     }
 
